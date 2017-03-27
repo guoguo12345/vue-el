9c89735cab8a71324a8b1477893a4eba2cb08545
guoguo <guoguo@qq.com>
2017-03-27 20:40:45 +0800

    17327

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class='header'>
+      header
+    </div>
+    <div class='tab'>
+      tab
+    </div>
+    <div class='content'>
+      content
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
